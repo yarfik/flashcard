7492cfae40064ed897d4adaddda89b45c5e1f667
yarfik <yarfik@gmail.com>
2024-06-20 23:28:32 +0700

    update

--- main.py
+++ main.py
@@ -1,28 +1,75 @@
-import tkinter as tk
+from tkinter import *
+from pandas import *
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+FOREIGN_LANG = "France"
+BASE_LANG = "English"
+random_word = {}
+timeout = ""
 
-window = tk.Tk()
-window.config(width=900, height=600, padx=50, pady=50, background=BACKGROUND_COLOR)
-window.title("Flash Card")
+# --------------------------------- data -------------------------------------
+dt_frame = read_csv('./data/french_words.csv')
+dictionary = dt_frame.to_dict(orient="records")
 
-card_image = tk.PhotoImage(file="./images/card_front.png")
-canvas = tk.Canvas(width=800, height=526, highlightthickness=0)
+root = Tk()
+root.config(width=900, height=600, padx=50, pady=50, background=BACKGROUND_COLOR)
+root.title("Flash Card")
+
+front_image = PhotoImage(file="./images/card_front.png")
+back_image = PhotoImage(file="./images/card_back.png")
+canvas = Canvas(width=800, height=526, highlightthickness=0)
 canvas.config(background=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=card_image)
+flashcard = canvas.create_image(400, 263, image=front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
 txt_title = canvas.create_text(400, 150, text="title", font=("Arial", 40, "italic"))
 txt_word = canvas.create_text(400, 263, text="title", font=("Arial", 60, "bold"))
 
-img_wrong = tk.PhotoImage(file="./images/wrong.png")
-button_wrong = tk.Button(image=img_wrong, highlightthickness=0)
-button_wrong.config(relief="flat", background=BACKGROUND_COLOR)
+
+def pick_random_word():
+    return random.choice(dictionary)
+
+
+def flip_card():
+    global random_word
+    canvas.itemconfig(flashcard, image=back_image)
+    canvas.itemconfig(txt_title, text=BASE_LANG, fill="white")
+    canvas.itemconfig(txt_word, text=random_word[BASE_LANG], fill="white")
+    root.after_cancel(timeout)
+
+
+def set_front_face(txt):
+    global timeout
+    canvas.itemconfig(flashcard, image=front_image)
+    canvas.itemconfig(txt_title, text=FOREIGN_LANG, fill="black")
+    canvas.itemconfig(txt_word, text=txt, fill="black")
+    timeout = root.after(ms=3000, func=flip_card)
+
+
+# def set_back_face(txt):
+#     canvas.itemconfig(txt_title, text=BASE_LANG)
+#     canvas.itemconfig(txt_word, text=txt)
+
+
+def next_card():
+    global random_word
+    random_word = pick_random_word()
+    set_front_face(random_word["French"])
+
+
+img_wrong = PhotoImage(file="./images/wrong.png")
+button_wrong = Button(image=img_wrong, highlightthickness=0)
+button_wrong.config(relief="flat", background=BACKGROUND_COLOR, command=next_card)
 button_wrong.grid(row=1, column=0)
 
-img_right = tk.PhotoImage(file="./images/right.png")
-button_right = tk.Button(image=img_right, highlightthickness=0)
-button_right.config(relief="flat", background=BACKGROUND_COLOR)
+img_right = PhotoImage(file="./images/right.png")
+button_right = Button(image=img_right, highlightthickness=0)
+button_right.config(relief="flat", background=BACKGROUND_COLOR, command=next_card)
 button_right.grid(row=1, column=1)
 
-window.mainloop()
+next_card()
+
+# print(dictionary)
+
+root.mainloop()
